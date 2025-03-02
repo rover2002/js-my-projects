0cbb1f959de10c30dca9d20f544e6ccc76cc3062
rover2002 <134366150+rover2002@users.noreply.github.com>
2025-03-02 12:28:08 +0000

    DataType

---
+++ .devcontainer/01_basic/02_DataTypes.js
@@ -0,0 +1,25 @@
+"use stricts"; //treat all JS code as newer version
+
+//alert( 3 + 3) // we are using NodeJS not browsers
+
+console.log(3 + 3) // code readability should be high
+
+console.log("Anmol")
+
+let name = "Anmol"
+let age = 18
+let isLoggedIn = false
+let state;
+// number => 2 to power 53 (range of numbers in memory Address)
+// bigint (use for Big Value of intigers for increase number range in Memory Address)
+// string => "" (use can be single or double but preferable is Doublese "")
+// boolean => true/false 
+// null => StandAlone Value
+// undefined =>(when value is not no define )
+// symbols => unique
+
+
+// object
+
+console.log(typeof null);
+console.log(typeof undefined); 
